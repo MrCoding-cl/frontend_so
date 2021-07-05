94ec9aa9c033910100e7fcd03d0cb53c53d123f3
gabriel <gbordesc@uft.edu>
2021-07-04 22:12:41 -0400

    react router

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,9 +3,10 @@ import "./../styles/global.scss";
 import Navbar from "./../components/Navbar";
 import IndexPage from "./index";
 import UberPage from "./uber";
-import { Switch, Route, Router } from "./../util/router.js";
+import { Switch, Route, Router} from "./../util/router.js";
 import NotFoundPage from "./not-found.js";
 import "./../util/analytics.js";
+import {Redirect} from "react-router-dom";
 
 //Context
 
@@ -25,8 +26,17 @@ function App(props) {
               spaced={true}
               logo="https://uft.cl/images/la_universidad/imagen-corporativa/Institucional/Logo_FINIS_institucional.png"
           />
-
           <Switch>
+            <Route
+                exact
+                path="/"
+                render={() => {
+                  return (
+                      <Redirect to="/frontend_so" />
+
+                  )
+                }}
+            />
             <Route exact path="/frontend_so" component={IndexPage} />
 
             <Route exact path="/frontend_so/uber" component={UberPage} />
